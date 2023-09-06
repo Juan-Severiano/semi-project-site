c81440ff0cd856c9560ffe53cd2c4566588b39d5
juan <juansoussev@gmail.com>
2023-09-05 22:03:09 -0300

    Update in the Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './styles/global.css'
 export default function App() {
   return (
     <>
+      <div className="fundo-video">
+        <img className='video' src="/public/bg-image.jpg" alt="asd"></img>
+      </div>
+
     <Header />
     </>
   )
